31b20c79b3ae30581053ca3a280a956eaf5ddde6
MohamedAliBadawy <medooali6262@gmail.com>
2025-05-28 17:21:51 +0300

    fixed add post image upload bug

--- lib/features/home/add_post.dart
+++ lib/features/home/add_post.dart
@@ -4,8 +4,7 @@ import 'package:ecommerece_app/core/helpers/spacing.dart';
 import 'package:ecommerece_app/core/theming/colors.dart';
 import 'package:ecommerece_app/core/theming/styles.dart';
 import 'package:ecommerece_app/features/auth/signup/data/models/user_model.dart';
-import 'package:ecommerece_app/features/auth/signup/data/signup_functions.dart'
-    as sp;
+
 import 'package:ecommerece_app/features/home/data/home_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -181,7 +180,7 @@ class _AddPostState extends State<AddPost> {
                         onTap: () async {
                           showLoadingDialog(context);
 
-                          imgUrl = await sp.uploadImageToFirebaseStorage();
+                          imgUrl = await uploadImageToFirebaseStorage();
                           Navigator.pop(context);
                           setState(() {});
                         },
