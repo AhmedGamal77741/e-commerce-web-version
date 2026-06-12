11bf4b1c1e381ce35c99d0695ad5a2ce146da7f5
MohamedAliBadawy <medooali6262@gmail.com>
2026-06-12 20:39:03 +0200

    Bug fix: when I upload a photo to a new post like this, there's a bug where the photos in the posts that were uploaded previously also get changed to the same photo

--- lib/features/home/data/home_functions.dart
+++ lib/features/home/data/home_functions.dart
@@ -308,9 +308,12 @@ Future<List<String>> uploadMultipleImagesToFirebaseHome() async {
 
     List<String> downloadUrls = await Future.wait(
       images.map((image) async {
-        DateTime.now().millisecondsSinceEpoch.toString();
-        final String fileName =
-            '\${timestamp}_\${images.indexOf(image)}_\$uid.jpg';
+        final String timestamp =
+            DateTime.now().millisecondsSinceEpoch.toString();
+        final String uid =
+            FirebaseAuth.instance.currentUser?.uid ?? 'anonymous';
+        final int index = images.indexOf(image);
+        final String fileName = '${timestamp}_${index}_$uid.jpg';
 
         final Uint8List rawBytes = await image.readAsBytes();
 
@@ -352,7 +355,7 @@ Future<List<String>> uploadMultipleImagesToFirebaseHome() async {
 
     return downloadUrls;
   } catch (e) {
-    print('Error uploading multiple images: \$e');
-    throw Exception('Failed to upload images: \$e');
+    print('Error uploading multiple images: $e');
+    throw Exception('Failed to upload images: $e');
   }
 }
