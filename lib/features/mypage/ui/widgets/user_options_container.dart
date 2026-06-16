1f2b9bbda13265d19cc85f9bb49b5e6040489d2f
MohamedAliBadawy <medooali6262@gmail.com>
2026-06-16 19:20:28 +0200

    feat(mypage): update partner/store entry registration URL

--- lib/features/mypage/ui/widgets/user_options_container.dart
+++ lib/features/mypage/ui/widgets/user_options_container.dart
@@ -372,7 +372,7 @@ class _UserOptionsContainerState extends State<UserOptionsContainer>
 }
 
 void _launchPartnerPage() async {
-  final url = Uri.parse('https://tally.so/r/w5O556');
+  final url = Uri.parse('https://link.inpock.co.kr/pang2chocolate');
   if (await canLaunchUrl(url)) {
     await launchUrl(url);
   } else {
